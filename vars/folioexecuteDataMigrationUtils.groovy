b2fd8f307ec3e5541cb43f1e153ebd52a2b5887d
Arsen <arsen_atoyan@epam.com>
2024-02-26 04:33:42 +0400

    fx

--- vars/folioexecuteDataMigrationUtils.groovy
+++ vars/folioexecuteDataMigrationUtils.groovy
@@ -15,20 +15,20 @@ import org.folio.karate.teams.TeamAssignment
 def getMigrationTime(rancher_cluster_name,rancher_project_name,resultMap,srcInstallJson,dstInstallJson,totalTimeInMs,modulesLongMigrationTimeSlack,modulesMigrationFailedSlack,startMigrationTime,pgadminURL){
 
 
-    // srcInstallJson.each { item ->
-    //     def (fullModuleName, moduleName, moduleVersion) = (item.id =~ /^(.*)-(\d*\.\d*\.\d*.*)$/)[0]
-    //     resultMap[moduleName] = [srcVersion: moduleVersion]
-    // }
-
     srcInstallJson.each { item ->
-        if (item instanceof Map && item.containsKey('id')) {
-            def (fullModuleName, moduleName, moduleVersion) = (item.id =~ /^(.*)-(\d*\.\d*\.\d*.*)$/)[0]
-            resultMap[moduleName] = [srcVersion: moduleVersion]
-        } else {
-            // Handle cases where item does not have an 'id' property
-            println "Invalid item: $item"
-        }
+        def (fullModuleName, moduleName, moduleVersion) = (item.id =~ /^(.*)-(\d*\.\d*\.\d*.*)$/)[0]
+        resultMap[moduleName] = [srcVersion: moduleVersion]
     }
+
+    // srcInstallJson.each { item ->
+    //     if (item instanceof Map && item.containsKey('id')) {
+    //         def (fullModuleName, moduleName, moduleVersion) = (item.id =~ /^(.*)-(\d*\.\d*\.\d*.*)$/)[0]
+    //         resultMap[moduleName] = [srcVersion: moduleVersion]
+    //     } else {
+    //         // Handle cases where item does not have an 'id' property
+    //         println "Invalid item: $item"
+    //     }
+    // }
 
 
     dstInstallJson.each { item ->
