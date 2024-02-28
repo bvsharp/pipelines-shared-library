6ceed481acbdede9a493c8bb89d827f730e40141
Dmytro Moroz <dmytro_moroz@epam.com>
2024-02-29 01:18:48 +0200

    fixing slack notifications for schema comparison

--- vars/folioSchemaCompareUtils.groovy
+++ vars/folioSchemaCompareUtils.groovy
@@ -257,7 +257,7 @@ void sendSlackNotification(String slackChannel) {
       println("Debug: Sending Slack Notification Now")
       if (slackNotifications) {
         println("Debug: slackNotifications object exists")
-        slackNotifications.sendSchemaComparisonSlackNotification(messege:message, "#folioschemacompare", buildStatus)
+        slackNotifications.sendSchemaComparisonSlackNotification(messege:message, "#folioschemacompare", currentBuild.result)
       } else {
         println("Debug: slackNotifications object is null")
       }
