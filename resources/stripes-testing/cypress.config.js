6c516e34fd924853a63e8ef1ce4c7e2352ab5ec4
Bill Avramenko <Vasyl_Avramenko@epam.com>
2024-06-14 14:36:02 +0300

    RANCHER-1527. Changes were made for test purposes.17

--- resources/stripes-testing/cypress.config.js
+++ resources/stripes-testing/cypress.config.js
@@ -12,18 +12,18 @@ const registerReportPortalPlugin = require('@reportportal/agent-js-cypress/lib/p
 const delay = async ms => new Promise(res => setTimeout(res, ms));
 
 const reportportalOptions = {
-//  endpoint: 'https://poc-report-portal.ci.folio.org/api/v1',
-//  apiKey: 'karate_YTzxxZQCTouIhffBDFYf9VFjLPdLn5sSumAN9Fs7SB64EIu3wrPFgbXHPc1OGs0Q',
+  endpoint: 'https://poc-report-portal.ci.folio.org/api/v1',
+  apiKey: 'karate_YTzxxZQCTouIhffBDFYf9VFjLPdLn5sSumAN9Fs7SB64EIu3wrPFgbXHPc1OGs0Q',
 //  launch: 'runNightlyCypressTests1271.109',
 //  launchId: '2e63f6f8-b1c3-4fff-ae5d-91395b5086d6',
-//  //CI_BUILD_ID: '106-1',
-//  project: 'cypress-nightly',
-//  description: 'CYPRESS scheduled tests',
-//  //autoMerge: true,
-//  //parallel: true,
-//  isLaunchMergeRequired: true,
-//  //reportHooks: true,
-//  debug: true,
+  //CI_BUILD_ID: '106-1',
+  project: 'cypress-nightly',
+  description: 'CYPRESS scheduled tests bla bla',
+  //autoMerge: true,
+  //parallel: true,
+  isLaunchMergeRequired: true,
+  //reportHooks: true,
+  debug: true,
   restClientConfig: {
     timeout: 360000,
   },
