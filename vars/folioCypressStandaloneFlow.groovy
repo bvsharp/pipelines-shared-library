9d5dd753bb242364c2c7f96f4e7128526d38fe13
PavloPak <pak.pavlo@gmail.com>
2024-06-24 16:08:24 +0300

    edit31

--- vars/folioCypressStandaloneFlow.groovy
+++ vars/folioCypressStandaloneFlow.groovy
@@ -107,7 +107,7 @@ void call(params) {
                   batch.each { workerNumber ->
                     def tagString = cypressTags.pop()
                     parallelWorkers["Worker#${workerNumber}"] = {
-                      stage(agetagStringnt) {
+                      stage(tagString) {
                         dir("cypress-${workerNumber}") {
                         executeTests(cypressImageVersion, "parallel_${customBuildName}"
                           , browserName, parallelExecParameters
